e057c46f07bec400819ed784ebab72cc36c7153b
zhong zheng <minechung@163.com>
2021-03-26 10:27:02 +0800

    新增人机验证

--- js/login.js
+++ js/login.js
@@ -6,15 +6,12 @@ var mreg = new RegExp(/^1[345789]\d{9}$/)
 
 $('.innertime').click(() => {
 
-    var data = {
-        mobile: $('#phonenum').val(),
-        type: 'login'
-    }
+
     if (!mreg.test($('#phonenum').val())) {
         $('.notice').html(
             `
             <div class="alert alert-danger" role="alert">请输入正确的电话号码</div>  
-            
+
             `
         )
         setTimeout(() => {
@@ -22,50 +19,7 @@ $('.innertime').click(() => {
         }, 2500)
         return false
     }
-    $.ajax({
-        url: baseurl + 'verification/code',
-        type: "POST",
-        data: data,
-        success: function(data) {
-            var timecode = 60
-            if (data.code == 200) {
-                $('.time').show()
-                $('.innertime').hide()
-                var timer = setInterval(() => {
-                    if (timecode == 0) {
-                        $('.time').hide()
-                        $('.innertime').show()
-                        clearInterval(timer)
-                        timecode = 60
-                        $('.time').html('60s')
-                    } else {
-                        timecode = timecode - 1
-                        $('.time').html(timecode + 's')
-
-
-                    }
-
-
-                }, 1000);
-            }
-
-        },
-        error: function(err) {
-
-            if (err) {
-
-                $('.notice').html(
-                    `
-                    <div class="alert alert-danger" role="alert">${err.responseJSON.message}</div>  
-                    
-                    `
-                )
-                setTimeout(() => {
-                    $('.notice').html('')
-                }, 2500)
-            }
-        }
-    })
+    $('.innerconetent').html(`  <iframe style="z-index: 99999999999999999999;" width="100%" height="500px" src="./verification.html?scene=nc_message_h5&platform=web&language=zh" frameborder="0"></iframe>`)
 
 
 })
@@ -183,4 +137,68 @@ $('.login_in').click(() => {
             }
         }
     })
+
+
+
+
+
+})
+window.addEventListener('message', function(e) {
+
+    if (e) {
+        $('#exampleModal').modal('hide')
+    }
+    var data = {
+        mobile: $('#phonenum').val(),
+        type: 'login',
+        nc_sessionid: e.data.sessionid,
+        nc_token: e.data.nc_token,
+        nc_sig: e.data.sig
+    }
+    $.ajax({
+        url: baseurl + 'verification/code',
+        type: "POST",
+        data: data,
+        success: function(data) {
+            var timecode = 60
+            if (data.code == 200) {
+                $('.time').show()
+                $('.innertime').hide()
+                var timer = setInterval(() => {
+                    if (timecode == 0) {
+                        $('.time').hide()
+                        $('.innertime').show()
+                        clearInterval(timer)
+                        timecode = 60
+                        $('.time').html('60s')
+                    } else {
+                        timecode = timecode - 1
+                        $('.time').html(timecode + 's')
+
+
+                    }
+
+
+                }, 1000);
+            }
+
+        },
+        error: function(err) {
+
+            if (err) {
+
+                $('.notice').html(
+                    `
+                    <div class="alert alert-danger" role="alert">${err.responseJSON.message}</div>  
+
+                    `
+                )
+                setTimeout(() => {
+                    $('.notice').html('')
+                }, 2500)
+            }
+        }
+    })
+
+
 })
